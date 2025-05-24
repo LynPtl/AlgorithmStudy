4c4abb7ab4168222b36213b4381501c28a45e28f
LynPtl <ptlantern@gmail.com>
2025-05-25 05:35:47 +1000

    continue add

---
+++ 9021_redo/素数06.py
@@ -0,0 +1,58 @@
+from itertools import compress,accumulate
+from math import sqrt
+import operator
+from collections import defaultdict
+
+def single_factors(number):
+    '''
+    Returns the product of the prime divisors of "number"
+    (using each prime divisor only once).
+
+    You can assume that "number" is an integer at least equal to 2.
+
+    >>> single_factors(2)
+    2
+    >>> single_factors(4096)                 # 4096 == 2**12
+    2
+    >>> single_factors(85)                   # 85 == 5 * 17
+    85
+    >>> single_factors(10440125)             # 10440125 == 5**3 * 17**4
+    85
+    >>> single_factors(154)                  # 154 == 2 * 7 * 11
+    154
+    >>> single_factors(52399401037149926144) # 52399401037149926144 == 2**8 * 7**2 * 11**15
+    154
+    '''
+    # return 0
+    # REPLACE THE PREVIOUS LINE WITH YOUR CODE
+    dict = defaultdict(int)
+    for num in range(2,int(sqrt(number))+1):
+    #for num in range(2,number+1):
+        if number == 1:
+            break
+        while number % num == 0:
+            dict[num] = dict[num]+1
+            number = number//num
+    if number > 1:
+        dict[number] = dict[number] + 1
+    times = 1
+    for x in dict:
+        times = times * x
+    return times
+
+#sample answer
+"""
+    counter = defaultdict(int)
+    for factor in range(2, number + 1):
+        if number == 1:
+            break
+        while number % factor == 0:
+            counter[factor] += 1
+            number = number // factor
+    # 返回连乘的乘积
+    return prod(counter)
+"""
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
