38eac7ecb807786a252b205521db6dd6cd4cdc23
LynPtl <ptlantern@gmail.com>
2025-05-25 20:02:07 +1000

    add sample answer

--- 9021_redo/打印输出04.py
+++ 9021_redo/打印输出04.py
@@ -34,6 +34,27 @@ def rectangle(width, height):
             line = line[::-1]
         print(line)
     return
+
+#sample answer
+"""
+    start = ord('a')
+    line = ""
+    # 输出字母
+    for i in range(width * height):
+        line += chr(start + i % 26)
+
+    # 输出结果
+    for i in range(0, len(line), width):
+        # i 的取值范围是0, width, 2 * width, 3 * width, 4 * width ..
+        # 如果width 5, 则 i 的取值范围是 0, 5, 10, 15, 20, 25..
+        line_no = i // width
+        # 每行的输出内容
+        each_line = line[i: i + width]
+        if line_no % 2 == 0:
+            print(each_line)
+        else:
+            print(each_line[::-1])
+"""
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
