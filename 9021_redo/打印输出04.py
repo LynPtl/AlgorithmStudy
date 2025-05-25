2e0856ccb66214dac32f585b81338ec1f2514e28
LynPtl <ptlantern@gmail.com>
2025-05-25 19:53:30 +1000

    update printoutput04

--- 9021_redo/打印输出04.py
+++ 9021_redo/打印输出04.py
@@ -2,6 +2,7 @@
 # chr(e) returns the character encoded by e.
 
 from itertools import cycle
+import string
 def rectangle(width, height):
     '''
     Displays a rectangle by outputting lowercase letters, starting with a,
@@ -23,9 +24,16 @@ def rectangle(width, height):
     ijklmnopqrstuvwxy
     ponmlkjihgfedcbaz
     '''
-    print()
     # REPLACE THE PREVIOUS LINE WITH YOUR CODE
-    
+    cyc = cycle(string.ascii_lowercase)
+    for i in range(height):
+        line = ""
+        for j in range(width):
+            line += next(cyc)
+        if i % 2 != 0:
+            line = line[::-1]
+        print(line)
+    return
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
